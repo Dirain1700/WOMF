1500bcbd81ac586bf8832e0ebf316eba6fdc1930
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2023-08-20 16:45:08 +0000

    Update error detection script

--- postwebpack.js
+++ postwebpack.js
@@ -6,6 +6,6 @@ const filePath = path.resolve(__dirname, "index.js");
 
 const indexJS = fs.readFileSync(filePath, "utf-8");
 
-const errorDetectionScript = `window.onerror=(message,file,lineNo,colNo)=>alert("Please report this to developer:\\n"+[message,file,lineNo,colNo].join("\\n"));window.addEventListener("unhandledrejection",(event)=>alert("Please report this to developer:\\nUnhandled promise rejection. Reason: "+event.reason));`;
+const errorDetectionScript = `window.onerror=(message,file,lineNo,colNo)=>{if(message!=="Script error.")alert("Please report this to developer:\\n"+[message,file,lineNo,colNo].join("\\n"));},`;
 
 fs.writeFileSync(filePath, errorDetectionScript + indexJS);
